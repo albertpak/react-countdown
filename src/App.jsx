5fbc6e9b0744a76a7fd1eab7ed7db3406845a0d9
Albert Pak <al@albert-pak.com>
2017-08-26 16:45:13 -0500

    adding basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>Countdown to December 25, 2017</div>
+        <div className="App-title">Countdown to December 25, 2017</div>
         <div>
-          <div>14 days</div>
-          <div>30 hours</div>
-          <div>15 minutes</div>
-          <div>20 seconds</div>
+          <div className="Clock-days">14 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-minutes">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
         </div>
         <div>
           <input placeholder="new date" />
