f275a43f41b78c6f4b7b8cb96ebc318eee0de0b6
Albert Pak <al@albert-pak.com>
2017-08-26 17:12:34 -0500

    calculating time until end date

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: '',
+    };
+  }
+
+  changeDeadline() {
+    this.setState({ deadline: this.state.newDeadline });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-title">Countdown to December 25, 2017</div>
-        <div>
-          <div className="Clock-days">14 days</div>
-          <div className="Clock-hours">30 hours</div>
-          <div className="Clock-minutes">15 minutes</div>
-          <div className="Clock-seconds">20 seconds</div>
+        <div className="App-title">
+          Countdown to {this.state.deadline}
         </div>
+        <Clock deadline={this.state.deadline} />
         <div>
-          <input placeholder="new date" />
-          <button>Submit</button>
+          <input
+            onChange={event =>
+              this.setState({ newDeadline: event.target.value })}
+            placeholder="new date"
+          />
+          <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
     );
